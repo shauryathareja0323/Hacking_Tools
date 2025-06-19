8f44e61b2d4033e0488d5a4582d654672633a8c3
Shaurya Thareja <shauryathareja0323@gmail.com>
2025-06-19 13:19:46 +0530

    A normal pin cracker

---
+++ RubberDucky/Android_4_DigitPinCracker/Android_4_DigitPinCracker.ino
@@ -0,0 +1,14 @@
+#include "DigiKeyboard.h"
+void setup() {
+}
+
+void loop() {
+  DigiKeyboard.sendKeyStroke(0);
+  DigiKeyboard.delay(100);
+  DigiKeyboard.sendKeyStroke(KEY_W, MOD_CONTROL_LEFT);
+  DigiKeyboard.delay(100);
+  DigiKeyboard.sendKeyStroke(KEY_F4, MOD_ALT_LEFT);
+  DigiKeyboard.delay(100);
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+
+}
---
+++ RubberDucky/Android_Pin_Cracker_mine/Android_Pin_Cracker_mine.ino
@@ -0,0 +1,12 @@
+#include "DigiKeyboard.h"
+
+void setup() {
+}
+
+void loop() {
+  DigiKeyboard.delay(2000);
+
+  DigiKeyboard.println("0323");
+
+  DigiKeyboard.delay(60000);
+}
